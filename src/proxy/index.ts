19dc0c8bde095ee5d47afa76f1cbef9ae003423e
Maximilian Kern <maxkernw@gmail.com>
2018-10-17 17:09:05 +0200

    formatting

--- src/proxy/index.ts
+++ src/proxy/index.ts
@@ -1,36 +1,35 @@
 interface IDoor {
-	Open(): void;
-	Close(): void;
+        Open(): void;
+        Close(): void;
 }
 
 class Door implements IDoor {
-	Open(): void {
-		console.log("Opening door")
-	}
-	Close(): void {
-		console.log("Closing door")
-	}
+        Open(): void {
+                console.log("Opening door")
+        }
+        Close(): void {
+                console.log("Closing door")
+        }
 }
 
 class SecureDoor {
-	private _door: IDoor;
+        private _door: IDoor;
 
-	constructor(door: IDoor) {
-		this._door = door;
-	}
+        constructor(door: IDoor) {
+                this._door = door;
+        }
 
-	Open(password: string): void {
-		this.Authenticate(password) ? this._door.Open() : console.log("nope!")
-	}
+        Open(password: string): void {
+                this.Authenticate(password) ? this._door.Open() : console.log("nope!")
+        }
 
-	Close(): void {
-		this._door.Close()
-	}
-	private Authenticate(password: string): boolean {
-		return password == "qwe123";
-	}
+        Close(): void {
+                this._door.Close()
+        }
+        private Authenticate(password: string): boolean {
+                return password == "qwe123";
+        }
 }
-
 
 const door = new SecureDoor(new Door());
 door.Open("invalid");
